ade7afb9018930310a1854b6f0afdf7d48c94ee1
Jean Souza <jecrsouza@gmail.com>
2025-04-23 18:07:53 -0300

    Deleting testing files i forgot to remove at the time of git init

--- reference.py
+++
@@ -1,6 +0,0 @@
-for c in next(os.walk("commands"))[2]:
-    lib = __import__("commands."+c[:-3])
-    if hasattr(getattr(lib,c[:-3]),"Handler"):
-        pass
-    else:
-        print("o {} não e um comando :(".format(c))
